bdad7631413051470b95b9e8b67563e01b9378e4
Mohd Saquib <ans2sb@gmail.com>
2019-12-03 21:49:30 +0530

    updated Allowed Hosts

--- arwaecomm/settings.py
+++ arwaecomm/settings.py
@@ -8,7 +8,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['arwaecomm.herokuapp.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
