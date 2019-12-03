0fd5470adcda07d15e5a541a1d16e52575a86522
Mohd Saquib <ans2sb@gmail.com>
2019-12-03 22:46:31 +0530

    added database configuration

--- arwaecomm/settings.py
+++ arwaecomm/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'NAME': 'db9r1sf2n5q8pr',
         'USER': 'ikfmmqwfuywozn',
         'PASSWORD': '058093adb59bf98cda31d57891fa854bdada6a01c2725ab33c52cf12a0aec9ba',
-        'HOST': 'c2-54-227-249-108.compute-1.amazonaws.com',
+        'HOST': 'ec2-54-227-249-108.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
