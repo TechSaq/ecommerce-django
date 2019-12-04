da63e236e13d62b60bae6ecb4fba2da8b04821c3
Mohd Saquib <ans2sb@gmail.com>
2019-12-04 12:16:44 +0530

    fixed whitenoise v4.0 upgrade issues

--- arwaecomm/wsgi.py
+++ arwaecomm/wsgi.py
@@ -1,9 +1,7 @@
 import os
 
-from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'arwaecomm.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
