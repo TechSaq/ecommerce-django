ae3a4881a9cae6f39feeaa35eb71ef87962677cb
Mohd Saquib <ans2sb@gmail.com>
2019-12-04 10:10:20 +0530

    set debug false

--- arwaecomm/settings.py
+++ arwaecomm/settings.py
@@ -5,7 +5,7 @@ from decouple import config
 
 ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 
-DEBUG = True
+DEBUG = False
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
 ALLOWED_HOSTS = ['arwaecomm.herokuapp.com']
